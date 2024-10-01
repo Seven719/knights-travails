ab1814595d575c913d722a68812056d5fa8b7d02
Iulian Nichifor <73722884+Seven719@users.noreply.github.com>
2024-10-01 09:53:43 +0100

    Made findShortestPath function
    Removed directions argument from findShortestPath

--- knight.js
+++ knight.js
@@ -16,8 +16,25 @@ const knightMoves = (start, end) => {
     return;
   }
 
-  const path = findShortestPath(start, end, directions);
+  const path = findShortestPath(start, end);
 
   console.log(`You made it in ${path.length - 1} moves! Here's your path:`);
   path.forEach((position) => console.log(position));
+};
+
+const findShortestPath = (start, end) => {
+  const queue = [[start]];
+
+  const visited = new Set();
+  visited.add(start.toString());
+
+  while (queue.length > 0) {
+    const currentPath = queue.shift();
+    const currentPosition = currentPath[currentPath.length - 1];
+
+    if (currentPosition[0] === end[0] && currentPosition[1] === end[1])
+      return currentPath;
+
+    checkNextPositions(currentPosition, currentPath, queue, visited);
+  }
 };
